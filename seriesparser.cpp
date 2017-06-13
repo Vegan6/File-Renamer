92507b67835abf19e3240dd29b60a998c5dd599f
vegan6 <vegan6@gmail.com>
2017-06-12 21:27:31 -0600

    Errors in class SeriesParser

--- seriesparser.cpp
+++ seriesparser.cpp
@@ -1,6 +1,10 @@
 #include "seriesparser.h"
 
 std::string fileName;
+std::string seriesName;
+std::string episode;
+std::string season;
+std::string year;
 
 seriesparser::seriesparser(std::string FileName)
 {
@@ -8,29 +12,95 @@ seriesparser::seriesparser(std::string FileName)
 }
 
 //Normalize filename
-
-std::str seriesparser::NormalizeString(std::string FileName)
+std::string NormalizeString(std::string FileName)
 {
     // Replace . _  with space
-    Replace(&FileName, ".", " ");
-    Replace(&FileName, "_", " ");
+    ReplaceInMem(&FileName, ".", " ");
+    ReplaceInMem(&FileName, "_", " ");
 
     // Remove some normal add ons
+    //xvid 1080 720 eztv ettv
 
     return FileName;
 }
 
 //Parse out TV series name and year
+std::string GetSeriesNameInMem (std::string &fileName)
+{
+
+}
 
 //Parse out season, episode
-
-// String Replace Char
-static inline void Replace(std::string &str, const std::string& from, const std::string& to)
+/*Look for:
+ * Pattern1 S###E###, S##E##, S#E#
+ * Pattern2 ##x##
+ * Pattern3 101 (season 1 episode 1)
+ *
+ * Todo:
+ * ep##
+ * e01-03
+ * e01+02
+ * episode ##
+ * season ##
+ * 1.01 (Season 1 Episode 1)
+ * season 00
+ *
+*/
+bool setEpisodeSeason (std::string &fileName)
 {
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos)
+    std::regex
+            pattern1("S([0-9]{0,2}[1-9])E([0-9]{0,2}[1-9])", std::regex_constants::icase),
+            pattern2("([0-9]{0,2}[1-9])x([0-9]{0,2}[1-9])", std::regex_constants::icase);
+    std::smatch
+            match;
+    std::string
+            seasonEpisode;
+
+    // Check Pattern 1 S01E06
+    if (std::regex_search(fileName.begin(), fileName.end(), match, pattern1))
     {
-        str.replace(start_pos, from.length(), to);
+        seasonEpisode = std::string(match[0]);
+        std::smatch sMatch;
+        if (std::regex_search(seasonEpisode.begin(), seasonEpisode.end(), sMatch, std::regex("[0-9]{0,2}[1-9]")))
+        {
+            season = sMatch[0];
+            episode = smatch[1];
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    // Check Pattern 1 01x06
+    else if (std::regex_search(fileName.begin(), fileName.end(), match, pattern2))
+    {
+        seasonEpisode = std::string(match[0]);
+        std::smatch sMatch;
+        if (std::regex_search(seasonEpisode.begin(), seasonEpisode.end(), sMatch, "[0-9]{0,2}[1-9]"))
+        {
+            season = sMatch[0];
+            episode = smatch[1];
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
+
+// String Replace
+static inline void ReplaceInMem(std::string &text, const std::string& from, const std::string& to)
+{
+    std::size_t
+            start_pos = 0;
+
+    while((start_pos = text.find(from, start_pos)) != std::string::npos)
+    {
+        text.replace(start_pos, from.length(), to);
         start_pos += to.length();
     }
 }
--- seriesparser.h
+++ seriesparser.h
@@ -2,12 +2,18 @@
 #define SERIESPARSER_H
 #include <string>
 #include <iostream>
+#include <regex>
 
 
 class seriesparser
 {
 public:
     seriesparser(std::string FileName);
+    static inline void ReplaceInMem(std::string &str, const std::string& from, const std::string& to);
+
+private:
+    bool setEpisodeSeason (std::string &fileName);
+    std::string NormalizeString(std::string FileName);
 };
 
 #endif // SERIESPARSER_H
